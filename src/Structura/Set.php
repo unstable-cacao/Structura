01d4d46f55bc1ca25066d3b3a6691d1f4e0983ea
Marina Sherstuk <marinas@commpeak.com>
2017-08-24 16:34:09 +0300

    Added symmetric difference function definition

--- src/Structura/Set.php
+++ src/Structura/Set.php
@@ -234,6 +234,14 @@ class Set implements \IteratorAggregate, \ArrayAccess
 	}
 	
 	/**
+	 * @param array|Set|\Traversable ...$set
+	 */
+	public function symmetricDiff(...$set): void
+	{
+		
+	}
+	
+	/**
 	 * Retrieve an external iterator
 	 * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
 	 * @return \Traversable An instance of an object implementing <b>Iterator</b> or
