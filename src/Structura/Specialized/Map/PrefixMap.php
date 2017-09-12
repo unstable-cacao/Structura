09924e8d7d8151fa937ed47a3e42b2d0a21739ea
Marina Sherstuk <marinas@commpeak.com>
2017-09-12 10:57:47 +0300

    Implemented PrefixMap

--- src/Structura/Specialized/Map/PrefixMap.php
+++ src/Structura/Specialized/Map/PrefixMap.php
@@ -7,16 +7,44 @@ use Structura\Exceptions\StructuraException;
 
 class PrefixMap
 {
+    /** @var array */
+    private $map = [];
+    
+    /** @var int */
+    private $prefixLength;
+    
+    
+    private function getCoolingPrefix(string $string): string 
+    {
+        return substr($string, 0, $this->prefixLength);
+    }
+    
+    private function getFirstMatchedKey(string $fullString, string $coolingPrefix): ?string 
+    {
+        foreach ($this->map[$coolingPrefix] as $key => $item) 
+        {
+            if (substr($fullString, 0, strlen($key)) == $key)
+                return $key;
+        }
+        
+        return null;
+    }
+    
+    
 	public function __construct(int $prefixLength = 3)
 	{
 		if ($prefixLength < 1)
 			throw new StructuraException("Prefix must be greater then 1. Got $prefixLength instead");
+		
+		$this->prefixLength = $prefixLength;
 	}
 
 
 	public function has(string $prefix): bool
 	{
-		
+	    $coolingPrefix = $this->getCoolingPrefix($prefix);
+	    
+		return key_exists($coolingPrefix, $this->map) && key_exists($prefix, $this->map[$coolingPrefix]);
 	}
 	
 	/**
@@ -25,12 +53,13 @@ class PrefixMap
 	 */
 	public function get(string $prefix)
 	{
-		
+		return $this->has($prefix) ? $this->map[$this->getCoolingPrefix($prefix)][$prefix] : null;
 	}
 	
 	public function tryGet(string $prefix, &$value): bool
 	{
-		
+        $value = $this->get($prefix);
+        return $this->has($prefix);
 	}
 
 	/**
@@ -39,21 +68,34 @@ class PrefixMap
 	 */
 	public function add(string $prefix, $value)
 	{
-		
+		$this->map[$this->getCoolingPrefix($prefix)][$prefix] = $value;
 	}
 	
 	public function remove(string $prefix): bool
 	{
-		
+		if ($this->has($prefix))
+        {
+            unset($this->map[$this->getCoolingPrefix($prefix)][$prefix]);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
 	}
 
-	/**
-	 * @param string $longKey
-	 * @return mixed|null
-	 */
-	public function hasFor(string $longKey)
+	public function hasFor(string $longKey): bool
 	{
-		
+	    $coolingPrefix = $this->getCoolingPrefix($longKey);
+	    
+		if (!key_exists($coolingPrefix, $this->map))
+            return false;
+        else if (key_exists($longKey, $this->map[$coolingPrefix]))
+            return true;
+        else if ($this->getFirstMatchedKey($longKey, $coolingPrefix))
+            return true;
+        else
+            return false;
 	}
 	
 	/**
@@ -62,11 +104,19 @@ class PrefixMap
 	 */
 	public function find(string $longKey)
 	{
-		
+	    $coolingPrefix = $this->getCoolingPrefix($longKey);
+	    
+		if ($this->has($longKey))
+		    return $this->map[$coolingPrefix][$longKey];
+		else if ($this->hasFor($longKey))
+		    return $this->map[$coolingPrefix][$this->getFirstMatchedKey($longKey, $coolingPrefix)];
+		else
+		    return null;
 	}
 	
 	public function tryFind(string $longKey, &$value): bool
 	{
-		
+		$value = $this->find($longKey);
+		return $this->hasFor($longKey);
 	}
 }
--- tests/Structura/Specialized/Map/PrefixMapTest.php
+++ tests/Structura/Specialized/Map/PrefixMapTest.php
@@ -10,46 +10,46 @@ class PrefixMapTest extends TestCase
     public function test_has_PrefixExists_ReturnTrue()
     {
         $subject = new PrefixMap();
-        $subject->add('Objection', 1);
+        $subject->add('ClassName', 1);
         
-        self::assertTrue($subject->has('Objection'));
+        self::assertTrue($subject->has('ClassName'));
     }
     
     public function test_has_PrefixNotExists_ReturnFalse()
     {
         $subject = new PrefixMap();
     
-        self::assertFalse($subject->has('Objection'));
+        self::assertFalse($subject->has('ClassName'));
     }
     
     public function test_get_ItemExists_ReturnItem()
     {
         $subject = new PrefixMap();
-        $subject->add('Objection', 1);
+        $subject->add('ClassName', 1);
     
-        self::assertEquals(1, $subject->get('Objection'));
+        self::assertEquals(1, $subject->get('ClassName'));
     }
     
     public function test_get_ItemNotExists_ReturnNull()
     {
         $subject = new PrefixMap();
     
-        self::assertNull($subject->get('Objection'));
+        self::assertNull($subject->get('ClassName'));
     }
     
     public function test_tryGet_ItemExists_ReturnTrue()
     {
         $subject = new PrefixMap();
-        $subject->add('Objection', 1);
+        $subject->add('ClassName', 1);
     
-        self::assertTrue($subject->tryGet('Objection', $value));
+        self::assertTrue($subject->tryGet('ClassName', $value));
     }
     
     public function test_tryGet_ItemExists_SetsValue()
     {
         $subject = new PrefixMap();
-        $subject->add('Objection', 1);
-        $subject->tryGet('Objection', $value);
+        $subject->add('ClassName', 1);
+        $subject->tryGet('ClassName', $value);
     
         self::assertEquals(1, $value);
     }
@@ -58,23 +58,23 @@ class PrefixMapTest extends TestCase
     {
         $subject = new PrefixMap();
     
-        self::assertFalse($subject->tryGet('Objection', $value));
+        self::assertFalse($subject->tryGet('ClassName', $value));
     }
     
     public function test_add_SavesItem()
     {
         $subject = new PrefixMap();
-        $subject->add('Objection', 1);
+        $subject->add('ClassName', 1);
     
-        self::assertTrue($subject->has('Objection'));
+        self::assertTrue($subject->has('ClassName'));
     }
     
     public function test_add_PrefixShorterThanDefined_SavesItem()
     {
         $subject = new PrefixMap();
-        $subject->add('Ob', 1);
+        $subject->add('Cl', 1);
     
-        self::assertTrue($subject->has('Ob'));
+        self::assertTrue($subject->has('Cl'));
     }
     
     public function test_add_PrefixEmpty_SavesItem()
@@ -88,88 +88,88 @@ class PrefixMapTest extends TestCase
     public function test_remove_ItemExists_RemovesItem()
     {
         $subject = new PrefixMap();
-        $subject->add('Objection', 1);
-        $subject->remove('Objection');
+        $subject->add('ClassName', 1);
+        $subject->remove('ClassName');
     
-        self::assertFalse($subject->has('Objection'));
+        self::assertFalse($subject->has('ClassName'));
     }
     
     public function test_remove_ItemExists_ReturnTrue()
     {
         $subject = new PrefixMap();
-        $subject->add('Objection', 1);
+        $subject->add('ClassName', 1);
     
-        self::assertTrue($subject->remove('Objection'));
+        self::assertTrue($subject->remove('ClassName'));
     }
     
     public function test_remove_ItemNotExists_ReturnFalse()
     {
         $subject = new PrefixMap();
     
-        self::assertFalse($subject->remove('Objection'));
+        self::assertFalse($subject->remove('ClassName'));
     }
     
     public function test_hasFor_ItemExists_ReturnTrue()
     {
         $subject = new PrefixMap();
-        $subject->add('Objection', 1);
+        $subject->add('ClassName', 1);
     
-        self::assertTrue($subject->hasFor('Objection'));
+        self::assertTrue($subject->hasFor('ClassName'));
     }
     
     public function test_hasFor_SubStringExists_ReturnTrue()
     {
         $subject = new PrefixMap();
-        $subject->add('Objection', 1);
+        $subject->add('ClassName', 1);
     
-        self::assertTrue($subject->hasFor('Objection\LiteObject'));
+        self::assertTrue($subject->hasFor('ClassName\Name'));
     }
     
     public function test_hasFor_ItemNotExists_ReturnFalse()
     {
         $subject = new PrefixMap();
-        $subject->add('ObjectUnitTests', 1);
+        $subject->add('ClassUnitTests', 1);
     
-        self::assertFalse($subject->hasFor('Objection'));
+        self::assertFalse($subject->hasFor('ClassName'));
     }
     
     public function test_find_ItemExists_ReturnItem()
     {
         $subject = new PrefixMap();
-        $subject->add('Objection', 1);
+        $subject->add('ClassName', 1);
     
-        self::assertEquals(1, $subject->find('Objection\LiteObject'));
+        self::assertEquals(1, $subject->find('ClassName\Name'));
     }
     
     public function test_find_SubStringsExists_ReturnFirstSubstringedItem()
     {
         $subject = new PrefixMap();
-        $subject->add('Objection', 1);
-        $subject->add('Objection\Lite', 2);
+        $subject->add('ClassName', 1);
+        $subject->add('ClassName\Na', 2);
     
-        self::assertEquals(1, $subject->find('Objection\LiteObject'));
+        self::assertEquals(1, $subject->find('ClassName\Name'));
     }
     
     public function test_find_ItemNotExists_ReturnNull()
     {
         $subject = new PrefixMap();
     
-        self::assertNull($subject->find('Objection\LiteObject'));
+        self::assertNull($subject->find('ClassName\Name'));
     }
     
     public function test_tryFind_ItemExists_ReturnTrue()
     {
         $subject = new PrefixMap();
-        $subject->add('Objection', 1);
+        $subject->add('ClassName', 1);
     
-        self::assertTrue($subject->tryFind('Objection\LiteObject', $value));
+        self::assertTrue($subject->tryFind('ClassName\Name', $value));
     }
     
     public function test_tryFind_ItemExists_SetsValue()
     {
         $subject = new PrefixMap();
-        $subject->add('Objection', 1);
-        $subject->tryFind('Objection\LiteObject', $value);
+        $subject->add('ClassName', 1);
+        $subject->tryFind('ClassName\Name', $value);
     
         self::assertEquals(1, $value);
     }
@@ -178,6 +178,6 @@ class PrefixMapTest extends TestCase
     {
         $subject = new PrefixMap();
     
-        self::assertFalse($subject->tryFind('Objection\LiteObject', $value));
+        self::assertFalse($subject->tryFind('ClassName\Name', $value));
     }
 }
