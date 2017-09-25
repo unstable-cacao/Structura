d6ea6bb52aeb3531951113358ad1b1fd143408ee
Marina Sherstuk <marinas@commpeak.com>
2017-09-25 13:51:51 +0300

    Added function definitions to DynamicTypeIndex and SimpleIndex

--- src/Structura/Table/Index/DynamicTypeIndex.php
+++ src/Structura/Table/Index/DynamicTypeIndex.php
@@ -7,5 +7,78 @@ use Structura\Table\IIndex;
 
 class DynamicTypeIndex implements IIndex
 {
-
+	public function has($value): bool
+	{
+		// TODO: Implement has() method.
+	}
+	
+	public function hasAny(array $value): bool
+	{
+		// TODO: Implement hasAny() method.
+	}
+	
+	public function hasAll(array $value): bool
+	{
+		// TODO: Implement hasAll() method.
+	}
+	
+	public function add(int $rowID, $value): IIndex
+	{
+		// TODO: Implement add() method.
+	}
+	
+	public function addBulk(array $valueByRowID): IIndex
+	{
+		// TODO: Implement addBulk() method.
+	}
+	
+	/**
+	 * @param mixed[] $valueByRowID
+	 * @return IIndex
+	 */
+	public function remRowIDs(array $valueByRowID): IIndex
+	{
+		// TODO: Implement remRowIDs() method.
+	}
+	
+	/**
+	 * @param mixed $value
+	 * @return int[] Removed row IDs
+	 */
+	public function remValue($value): array
+	{
+		// TODO: Implement remValue() method.
+	}
+	
+	/**
+	 * @param array $value
+	 * @return int[] Removed row IDs
+	 */
+	public function remValues(array $value): array
+	{
+		// TODO: Implement remValues() method.
+	}
+	
+	/**
+	 * @param mixed $value
+	 * @return int[]
+	 */
+	public function findValue($value): array
+	{
+		// TODO: Implement findValue() method.
+	}
+	
+	/**
+	 * @param mixed[] $values
+	 * @return int[][] Values matching the same index as value index.
+	 */
+	public function findValues(array $values): array
+	{
+		// TODO: Implement findValues() method.
+	}
+	
+	public function clear(): void
+	{
+		// TODO: Implement clear() method.
+	}
 }
