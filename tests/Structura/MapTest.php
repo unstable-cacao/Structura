58823c3fe6557e5737111b750f351f9c83293678
Marina Sherstuk <marinas@commpeak.com>
2017-10-03 18:00:50 +0300

    Started implementing unit tests

---
+++ tests/Structura/MapTest.php
@@ -0,0 +1,250 @@
+<?php
+namespace Structura;
+
+
+use PHPUnit\Framework\TestCase;
+
+
+class MapTest extends TestCase
+{
+	/**
+	 * @expectedException \Structura\Exceptions\StructuraException
+	 */
+	public function test_add_KeyNotValid_ExceptionThrown()
+	{
+		$subject = new Map();
+		$subject->add([], 1);
+	}
+	
+	public function test_add_KeyValid_AddValueToMap()
+	{
+		$subject = new Map();
+		$subject->add(1, 1);
+		
+		self::assertTrue($subject->has(1));
+	}
+	
+	public function test_add_KeyExists_OverwriteOldValue()
+	{
+		$subject = new Map();
+		$subject->add(1, 1);
+		$subject->add(1, 2);
+		
+		self::assertEquals(2, $subject->get(1));
+	}
+	
+	/**
+	 * @expectedException \Structura\Exceptions\StructuraException
+	 */
+	public function test_remove_KeyNotValid_ExceptionThrown()
+	{
+		$subject = new Map();
+		$subject->remove([]);
+	}
+	
+	public function test_remove_KeyNotExists_Ignore()
+	{
+		$subject = new Map();
+		$subject->add(2, 2);
+		$subject->remove(1);
+		
+		self::assertEquals(1, $subject->count());
+	}
+	
+	public function test_remove_KeyExists_RemoveFromMap()
+	{
+		$subject = new Map();
+		$subject->add(1, 1);
+		$subject->remove(1);
+		
+		self::assertFalse($subject->has(1));
+	}
+	
+	/**
+	 * @expectedException \Structura\Exceptions\StructuraException
+	 */
+	public function test_get_KeyNotValid_ExceptionThrown()
+	{
+		$subject = new Map();
+		$subject->get([]);
+	}
+	
+	/**
+	 * @expectedException \Structura\Exceptions\StructuraException
+	 */
+	public function test_get_KeyNotExists_ExceptionThrown()
+	{
+		$subject = new Map();
+		$subject->get(1);
+	}
+	
+	public function test_get_KeyExists_ReturnValue()
+	{
+		$subject = new Map();
+		$subject->add(1, 1);
+		
+		self::assertEquals(1, $subject->get(1));
+	}
+	
+	/**
+	 * @expectedException \Structura\Exceptions\StructuraException
+	 */
+	public function test_tryGet_KeyNotValid_ExceptionThrown()
+	{
+		$subject = new Map();
+		$subject->tryGet([], $value);
+	}
+	
+	public function test_tryGet_KeyExists_ReturnTrue()
+	{
+		$subject = new Map();
+		$subject->add(1, 1);
+		
+		self::assertTrue($subject->tryGet(1, $value));
+	}
+	
+	public function test_tryGet_KeyExists_SetsValue()
+	{
+		$subject = new Map();
+		$subject->add(1, 1);
+		$subject->tryGet(1, $value);
+		
+		self::assertEquals(1, $value);
+	}
+	
+	public function test_tryGet_KeyNotExists_ReturnFalse()
+	{
+		$subject = new Map();
+		
+		self::assertFalse($subject->tryGet(1, $value));
+	}
+	
+	public function test_tryGet_KeyNotExists_SetsNull()
+	{
+		$subject = new Map();
+		$subject->tryGet(1, $value);
+		
+		self::assertNull($value);
+	}
+	
+	/**
+	 * @expectedException \Structura\Exceptions\StructuraException
+	 */
+	public function test_has_KeyNotValid_ExceptionThrown()
+	{
+		$subject = new Map();
+		$subject->has([]);
+	}
+	
+	public function test_has_KeyExists_ReturnTrue()
+	{
+		
+	}
+	
+	public function test_has_KeyNotExists_ReturnFalse()
+	{
+		
+	}
+	
+	public function test_hasAny_NoneExist_ReturnFalse()
+	{
+		
+	}
+	
+	public function test_hasAny_SomeExist_ReturnTrue()
+	{
+		
+	}
+	
+	public function test_hasAny_AllExist_ReturnTrue()
+	{
+		
+	}
+	
+	public function test_hasAll_NoneExist_ReturnFalse()
+	{
+		
+	}
+	
+	public function test_hasAll_SomeExist_ReturnFalse()
+	{
+		
+	}
+	
+	public function test_hasAll_AllExist_ReturnTrue()
+	{
+		
+	}
+	
+	public function test_count_MapEmpty_ReturnZero()
+	{
+		$subject = new Map();
+		
+		self::assertEquals(0, $subject->count());
+	}
+	
+	public function test_count_MapNotEmpty_ReturnCount()
+	{
+		$subject = new Map();
+		$subject->add(1, 1);
+		$subject->add(2, 2);
+		$subject->add(3, 3);
+		
+		self::assertEquals(3, $subject->count());
+	}
+	
+	public function test_clear_Clears()
+	{
+		$subject = new Map();
+		$subject->add(1, 1);
+		$subject->add(2, 2);
+		$subject->add(3, 3);
+		$subject->clear();
+		
+		self::assertEquals(0, $subject->count());
+	}
+	
+	public function test_getIterator_ForeachPossible()
+	{
+		
+	}
+	
+	public function test_offsetExists_Exists_ReturnTrue()
+	{
+		
+	}
+	
+	public function test_offsetExists_NotExists_ReturnFalse()
+	{
+		
+	}
+	
+	public function test_offsetGet_Exists_GetsFromMap()
+	{
+		
+	}
+	
+	/**
+	 * @expectedException \Structura\Exceptions\StructuraException
+	 */
+	public function test_offsetGet_NotExists_ExceptionThrown()
+	{
+		$subject = new Map();
+		$subject[[]];
+	}
+	
+	public function test_offsetSet_SetsToMap()
+	{
+		
+	}
+	
+	public function test_offsetSet_AlreadyExists_OverwritesOldValue()
+	{
+		
+	}
+	
+	public function test_offsetUnset_RemovesFromMap()
+	{
+		
+	}
+}
