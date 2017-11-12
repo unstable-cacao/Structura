c0b8806377f1b1a5184d155da11c76117fcdd224
Alexey Puchkov <alexey.pkv@gmail.com>
2017-11-12 08:18:59 +0200

    Create initial abstrction for the Arrays module

---
+++ src/Structura/Arrays.php
@@ -0,0 +1,47 @@
+<?php
+namespace Structura;
+
+
+class Arrays
+{
+	public static function asArray($data): array
+	{
+		
+	}
+	
+	public static function toArray(&$data): array
+	{
+		$data = self::asArray($data);
+		return $data;
+	}
+	
+	public static function first(array $data)
+	{
+		
+	}
+	
+	public static function last(array $data)
+	{
+		
+	}
+	
+	public static function isNumeric(array $data): bool
+	{
+		
+	}
+	
+	public static function isAssoc(array $data): bool
+	{
+		
+	}
+	
+	public static function firstKey(array $data)
+	{
+		
+	}
+	
+	public static function lastKey(array $data)
+	{
+		
+	}
+}
