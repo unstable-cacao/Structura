9dd279b608f39835b9e40e0b5e0e7b564a253b4d
Alexey Puchkov <alexey.pkv@gmail.com>
2018-12-06 10:39:11 +0200

    Add some properties and Vertex

--- src/Structura/Graph/Node.php
+++ src/Structura/Graph/Node.php
@@ -22,5 +22,69 @@ class Node
 		
 	}
 	
+	/**
+	 * @param Node|Node[] $with
+	 * @param mixed|null $data
+	 */
+	public function connect($with, $data = null): void
+	{
+		
+	}
 	
+	/**
+	 * @param Node|IIdentifiable|string $to
+	 * @return bool
+	 */
+	public function isConnectedTo($to): bool
+	{
+		
+	}
+	
+	/**
+	 * @param Node|IIdentifiable|string $with
+	 * @return bool
+	 */
+	public function getVertexData($with): bool
+	{
+		
+	}
+	
+	/**
+	 * @return Node[]
+	 */
+	public function getConnected(): array
+	{
+		
+	}
+	
+	/**
+	 * @param Node|IIdentifiable|string $with
+	 * @return null|Vertex
+	 */
+	public function getVertex($with): ?Vertex
+	{
+		
+	}
+	
+	/**
+	 * @param Node[]|IIdentifiable[]|string[] $with
+	 * @return array
+	 */
+	public function getVertexes(array $with): array
+	{
+		
+	}
+	
+	/**
+	 * @param Node|IIdentifiable|string|[]|IIdentifiable[]|string[] $from
+	 */
+	public function disconnect($from): void
+	{
+		
+	}
+	
+	public function connectionsCount(): int
+	{
+		
+	}
 }
---
+++ src/Structura/Graph/Vertex.php
@@ -0,0 +1,50 @@
+<?php
+namespace Structura\Graph;
+
+
+class Vertex
+{
+	public function __construct(Node $a, Node $b, $data)
+	{
+	}
+	
+	
+	/**
+	 * @return mixed
+	 */
+	public function getData()
+	{
+		
+	}
+	
+	
+	public function setData($data): void
+	{
+		
+	}
+	
+	public function left(): Node
+	{
+		
+	}
+	
+	public function right(): Node
+	{
+		
+	}
+	
+	public function nodes(): array
+	{
+		
+	}
+	
+	public function has(Node $n): bool
+	{
+		
+	}
+	
+	public function isBetween(Node $n1, Node $n2): bool
+	{
+		
+	}
+}
