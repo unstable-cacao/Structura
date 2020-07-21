ab4583ac837c91dd75d417a31656628d1a269bba
Alexey Puchkov <alexey.pkv@gmail.com>
2020-07-21 09:27:42 +0300

    Prevent function redifinition in structura

--- src/time.php
+++ src/time.php
@@ -3,16 +3,22 @@ define('START_OF_TIME', '1970-01-01 00:00:00');
 define('END_OF_TIME', '3999-12-31 23:59:59');
 
 
-/**
- * @param string|int|null $value
- * @return string
- */
-function get_time($value = null): string
+if (!function_exists('get_time'))
 {
-	return \Structura\Time::get($value);
+	/**
+	 * @param string|int|null $value
+	 * @return string
+	 */
+	function get_time($value = null): string
+	{
+		return \Structura\Time::get($value);
+	}
 }
 
-function now(): string
+if (!function_exists('now'))
 {
-	return \Structura\Time::now();
+	function now(): string
+	{
+		return \Structura\Time::now();
+	}
 }
