ae757640b173caf94f1c9c2a2c417756d7f6b07d
Alexey Puchkov <alexey.pkv@gmail.com>
2020-07-21 09:38:28 +0300

    Don't redefined START_OF_TIME and END_OF_TIME

--- src/time.php
+++ src/time.php
@@ -1,17 +1,18 @@
 <?php
-define('START_OF_TIME', '1970-01-01 00:00:00');
-define('END_OF_TIME', '3999-12-31 23:59:59');
+if (!defined('START_OF_TIME')) define('START_OF_TIME', '1970-01-01 00:00:00');
+if (!defined('END_OF_TIME')) define('END_OF_TIME', '3999-12-31 23:59:59');
 
 
 if (!function_exists('get_time'))
 {
 	/**
 	 * @param string|int|null $value
+	 * @param string $format
 	 * @return string
 	 */
-	function get_time($value = null): string
+	function get_time($value = null, string $format = \Structura\Time::DEFAULT_FORMAT): string
 	{
-		return \Structura\Time::get($value);
+		return \Structura\Time::get($value, $format);
 	}
 }
 
