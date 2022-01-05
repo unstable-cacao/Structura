ec0e9db38985286c48be7b199df2d656f58ef035
Marina Puchkov <marinashers@gmail.com>
2022-01-05 17:09:45 +0200

    Implemented weekOfYear in Time

--- src/Structura/Time.php
+++ src/Structura/Time.php
@@ -27,6 +27,65 @@ class Time
 		}
 	}
 	
+	private static function startOfFirstWeek(string $date): string
+	{
+		$firstDay = self::dayOfWeek($date);
+		$result = $date;
+		
+		if ($firstDay >= 4)
+		{
+			$result = self::get("$date next sunday", 'Y-m-d');
+		}
+		else if ($firstDay > 0)
+		{
+			$result = self::get("$date last sunday", 'Y-m-d');
+		}
+		
+		return $result;
+	}
+	
+	
+	protected static function dayOfWeek(string $date): int
+	{
+		return self::get($date, 'w');
+	}
+	
+	protected static function weekOfYearMondayBased(string $date): int
+	{
+		return self::get($date, 'W');
+	}
+	
+	protected static function weekOfYearSundayBased(string $date): int
+	{
+		$firstDate = self::get($date, 'Y-01-01');
+		$firstDateLastYear = self::get("$firstDate -1 year", 'Y-m-d');
+		$firstDateNextYear = self::get("$firstDate +1 year", 'Y-m-d');
+		
+		$formatted = self::get($date, 'Y-m-d');
+		
+		$firstDate = self::startOfFirstWeek($firstDate);
+		$firstDateNextYear = self::startOfFirstWeek($firstDateNextYear);
+		
+		// for example 2000-12-31 should be part of next year
+		if ($formatted >= $firstDateNextYear)
+		{
+			$firstDate = $firstDateNextYear;
+		}
+		// for example 2000-01-01 should be part of last year
+		else if ($firstDate > $formatted)
+		{
+			$firstDate = self::startOfFirstWeek($firstDateLastYear);
+		}
+		
+		$timestamp = strtotime($formatted);
+		$firstTimestamp = strtotime($firstDate);
+		
+		$seconds = $timestamp - $firstTimestamp;
+		$days = $seconds / (60 * 60 * 24);
+		
+		return floor($days / 7) + 1;
+	}
+	
 	
 	public static function now(): string
 	{
@@ -42,5 +101,13 @@ class Time
 	{
 		$time = self::formatTime($time);
 		return date($format, $time);
+	}
+	
+	public static function weekOfYear(string $date = 'now', bool $isSundayBased = false): int
+	{
+		if ($isSundayBased)
+			return self::weekOfYearSundayBased($date);
+		else
+			return self::weekOfYearMondayBased($date);
 	}
 }
--- tests/Structura/TimeTest.php
+++ tests/Structura/TimeTest.php
@@ -7,6 +7,79 @@ use PHPUnit\Framework\TestCase;
 
 class TimeTest extends TestCase
 {
+	private const WEEKS_TEST = [
+		// [ expected week, given date ]
+		[52, '2000-01-01'],
+		[1, '2000-01-02'],
+		[1, '2000-01-03'],
+		[1, '2000-01-07'],
+		[1, '2000-01-08'],
+		[5, '2000-01-31'],
+		[31, '2000-08-01'],
+		[34, '2000-08-22'],
+		[1, '2000-12-31'],
+		
+		[1, '2001-01-01'],
+		[1, '2001-01-02'],
+		[2, '2001-01-08'],
+		[3, '2001-01-14'],
+		[52, '2001-12-29'],
+		[1, '2001-12-30'],
+		[1, '2001-12-31'],
+		
+		[1, '2002-01-01'],
+		[1, '2002-01-02'],
+		[2, '2002-01-07'],
+		[3, '2002-01-13'],
+		[3, '2002-01-13'],
+		[52, '2002-12-28'],
+		[1, '2002-12-29'],
+		[1, '2002-12-31'],
+		
+		[1, '2003-01-01'],
+		[1, '2003-01-02'],
+		[2, '2003-01-06'],
+		[3, '2003-01-12'],
+		[53, '2003-12-28'],
+		[53, '2003-12-31'],
+		
+		[53, '2004-01-01'],
+		[53, '2004-01-02'],
+		[1, '2004-01-04'],
+		[2, '2004-01-12'],
+		[3, '2004-01-18'],
+		[52, '2004-12-26'],
+		[52, '2004-12-31'],
+		
+		[52, '2005-01-01'],
+		[1, '2005-01-02'],
+		[2, '2005-01-09'],
+		[3, '2005-01-17'],
+		[52, '2005-12-31'],
+		
+		[1, '2006-01-01'],
+		[1, '2006-01-02'],
+		[2, '2006-01-12'],
+		[3, '2006-01-18'],
+		[52, '2006-12-30'],
+		[1, '2006-12-31'],
+		
+		[53, '2009-01-01'],
+		[53, '2009-01-03'],
+		[1, '2009-01-04'],
+		[1, '2009-01-05'],
+		[2, '2009-01-13'],
+		[52, '2009-12-27'],
+		[52, '2009-12-31'],
+		
+		[52, '2010-01-01'],
+		[52, '2010-01-02'],
+		[1, '2010-01-04'],
+		[2, '2010-01-11'],
+		[52, '2010-12-27'],
+		[52, '2010-12-31']
+	];
+	
 	private function generateTimeValues(callable $callback, &$result): string
 	{
 		$before = date('Y-m-d H:i:s');
@@ -81,5 +154,57 @@ class TimeTest extends TestCase
 	public function test_get_FormatNotDefault()
 	{
 		self::assertEquals('2010-01-01', Time::get('2010-01-01 05:55:01', 'Y-m-d'));
+	}
+	
+	public function test_weekOfYear_MondayBased()
+	{
+		self::assertEquals(1, Time::weekOfYear('2022-01-03'));
+		self::assertEquals(2, Time::weekOfYear('2022-01-16'));
+		self::assertEquals(51, Time::weekOfYear('2021-12-22'));
+		self::assertEquals(52, Time::weekOfYear('2022-01-01'));
+		self::assertEquals(53, Time::weekOfYear('2021-01-01'));
+	}
+	
+	public function test_weekOfYear_SundayFirst()
+	{
+		self::assertEquals(1, Time::weekOfYear('2006-01-01', true));
+	}
+	
+	public function test_weekOfYear_MondayFirst()
+	{
+		self::assertEquals(1, Time::weekOfYear('2001-01-01', true));
+	}
+	
+	public function test_weekOfYear_TuesdayFirst()
+	{
+		self::assertEquals(1, Time::weekOfYear('2002-01-01', true));
+	}
+	
+	public function test_weekOfYear_WednesdayFirst()
+	{
+		self::assertEquals(1, Time::weekOfYear('2003-01-01', true));
+	}
+	
+	public function test_weekOfYear_ThursdayFirst()
+	{
+		self::assertEquals(53, Time::weekOfYear('2009-01-01', true));
+	}
+	
+	public function test_weekOfYear_FridayFirst()
+	{
+		self::assertEquals(52, Time::weekOfYear('2010-01-01', true));
+	}
+	
+	public function test_weekOfYear_SaturdayFirst()
+	{
+		self::assertEquals(52, Time::weekOfYear('2005-01-01', true));
+	}
+	
+	public function test_weekOfYear()
+	{
+		foreach (self::WEEKS_TEST as $item)
+		{
+			self::assertEquals($item[0], Time::weekOfYear($item[1], true), "Failed asserting that week {$item[0]} is for {$item['1']}");
+		}
 	}
 }
